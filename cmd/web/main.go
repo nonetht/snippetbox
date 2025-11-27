d698be770c04de29437bb649735ccb58e1bcca04
yang <94279769+nonetht@users.noreply.github.com>
2025-11-27 18:19:23 +0800

    add fileServer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory
+	// Note that the path given to the http.Dir function is relative to the project
+	fileServer := http.FileServer(http.Dir("./ut/static/"))
+
+	// Use the mux.Handle() function to register the file server
+	// StripPrefix Remove the given prefix from the given URL
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other application routes as normal
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
