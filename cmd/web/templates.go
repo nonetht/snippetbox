34faececdb06c1348f3c741208602299330c9876
yang <94279769+nonetht@users.noreply.github.com>
2025-12-17 13:10:21 +0800

    added new newTemplateCache method

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,45 @@
 package main
 
-import "snippetbox.yang.net/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"snippetbox.yang.net/internal/models"
+)
 
 // 构建一个结构体，即 templateData 类型，去承载任意动态数据
 // 此时我们仅包含一个字段，但是后续会添加更多内容。
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Initialize a new map
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// Base return the last element of page, like "./ui/html/pages/home.tmpl" -> "home.tmpl"
+		name := filepath.Base(page)
+
+		// Create a slice containing the filepaths for our base
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	return cache, nil
 }
