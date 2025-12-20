93bb031250cd069bed0b676ba26596392b64b54c
yang <94279769+nonetht@users.noreply.github.com>
2025-12-20 18:16:45 +0800

    added slog and flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "http service address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	mux := http.NewServeMux()
 
-	// http.FileServer 将“设定的根目录”和“请求的URL路径”直接拼到一起去硬盘之中寻找文件。
+	// 将请求 URL 映射到 ./ui/static/ 目录下文件读出来并返回
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 
-	// Use the mux.Handle() function...
+	// 1.受到 GET /static/ 请求，随即交给 handler 处理
+	// 2. 将请求的开头 "/static" 去掉，比如请求是 /static/css/main.css，最后变成了 ./ui/static/css/main.csss
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
+	//logger.Info("request receivced", "method", "GET", "path", "/")
+
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server", "addr", addr)
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error()) // Use Error() method to log any **error message**
+	os.Exit(1)
 }
